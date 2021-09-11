f5e4dd68786f0780b4df3f6932b75febd333d944
Andrea D <andreadh01@gmail.com>
2021-09-10 18:58:56 -0700

    se agrego lo grafico a tracker

---
+++ app/src/main/java/com/example/sugartracker/InsertManually.java
@@ -0,0 +1,14 @@
+package com.example.sugartracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertManually extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_manually);
+    }
+}
--- app/src/main/java/com/example/sugartracker/TrackerActivity.java
+++ app/src/main/java/com/example/sugartracker/TrackerActivity.java
@@ -1,13 +1,26 @@
 package com.example.sugartracker;
 
+import static java.security.AccessController.getContext;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.text.style.TextAppearanceSpan;
+import android.text.style.TypefaceSpan;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -16,18 +29,46 @@ import com.google.android.material.navigation.NavigationBarView;
 public class TrackerActivity extends AppCompatActivity {
 
     private Button back_btn;
+    private Button diabsen_btn;
+    private Button manually_btn;
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
+        Typeface kanit = Typeface.create(ResourcesCompat.getFont(this, R.font.kanit_extralight), Typeface.NORMAL);
+        Typeface montserrat = Typeface.create(ResourcesCompat.getFont(this, R.font.montserrat), Typeface.BOLD);
+
 
         back_btn = findViewById(R.id.back);
+        diabsen_btn = findViewById(R.id.auto);
+        manually_btn = findViewById(R.id.manually);
+
+        Spannable spannable = new SpannableString("With DIABSEN device");
+        spannable.setSpan(new TypefaceSpan(montserrat), 0, 4,Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        spannable.setSpan(new TypefaceSpan(kanit), 5, 12,Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        spannable.setSpan(new TypefaceSpan(montserrat), 13, 19,Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        diabsen_btn.setText(spannable);
 
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        diabsen_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), InsertAuto.class));
+            }
+        });
+
+        manually_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), InsertManually.class));
             }
         });
 
